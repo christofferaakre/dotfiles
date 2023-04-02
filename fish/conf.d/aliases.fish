f8d82bbc57b87372151bb26727a2ad7636f1687d
christofferaakre <christoffer.corfield@gmail.com>
2023-04-02 18:21:37 +0200

    cleanup fish aliases

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,16 +1,7 @@
 alias aliases="vim ~/bin/dotfiles/fish/conf.d/aliases.fish"
 
-alias open="xdg-open"
-
-alias notes="pushd ~/notes && vim && popd"
-
-# cd
-alias bin="cd ~/bin"
-alias coding="cd ~/coding"
-
 # editor
 alias vim="nvim"
-#alias vi="nvim"
 alias hx="helix"
 alias code="code-insiders"
 
@@ -29,16 +20,3 @@ alias arc="vim ~/.config/awesome/rc.lua"
 
 # coreutil replacements
 alias ls="exa"
-
-# godot
-alias gdvim="nvim --listen godot"
-
-# gwa
-alias scripts="cd ~/Documents/scripts"
-
-# os
-alias qemu="qemu-system-x86_64"
-alias igcc="$HOME/opt/cross/bin/i686-elf-gcc"
-alias ild="$HOME/opt/cross/bin/i686-elf-ld"
-alias iobjdump="$HOME/opt/cross/bin/i686-elf-objdump"
-alias igdb="$HOME/opt/cross/bin/i686-elf-gdb"
