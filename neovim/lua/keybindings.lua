d00234f618e6b66d9d144ab9e52d1e48c1d1268a
christofferaakre <christoffer.corfield@gmail.com>
2023-04-02 22:05:29 +0200

    lots of new keybindings

--- neovim/lua/keybindings.lua
+++ neovim/lua/keybindings.lua
@@ -1,9 +1,26 @@
--- Easy split navigation
-map('n', '<C-J>', '<C-W>j', { noremap = true, silent = false})
-map('n', '<C-K>', '<C-W>k', { noremap = true, silent = false})
-map('n', '<C-L>', '<C-W>l', { noremap = true, silent = false})
-map('n', '<C-H>', '<C-W>h', { noremap = true, silent = false})
-
 -- Save on Ctrl+s
-map('n', '<C-S>', ':w<CR>', { noremap = true, silent = false})
+map('n', '<C-S>', ':w<CR>', { noremap = true, silent = false })
 
+-- Move lines around in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Put several lines on same line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Center cursor on <C-d> and <C-u>
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Center cursor on search
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- Quickfix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, noremap = true })
