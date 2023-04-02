4d2a6d7e2ab2bd0180beb78d9fc6e1023f6ad362
christofferaakre <christoffer.corfield@gmail.com>
2023-04-02 19:17:27 +0200

    colors

--- neovim/init.lua
+++ neovim/init.lua
@@ -2,11 +2,11 @@
 vim.g["python3_host_prog"] = "~/.local/venv/nvim/bin/python3"
 
 require("sets")
---require("plugins")
+require("plugins")
+require("colors")
 --
 --require("keybindings")
 --require("treesitter")
---require("colors")
 --require("lsp")
 --require("syntax_highlighting")
 --require("comment")
