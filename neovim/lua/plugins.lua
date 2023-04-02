918d0f18c025768eb7b8e1f4d48aa5d6c9e1d42f
christofferaakre <christoffer.corfield@gmail.com>
2023-04-02 19:41:06 +0200

    comment, treesitter

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -2,4 +2,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     use 'gruvbox-community/gruvbox'
+    use {'numToStr/Comment.nvim'}
+    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use 'nvim-treesitter/nvim-treesitter-context'
 end)
