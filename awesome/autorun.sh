7afc2335e2680ddd5893ccb966389ec9cbf4eb4a
Christoffer Aakre <christoffer.corfield@gmail.com>
2023-05-17 01:37:17 +0200

    starisgip

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-xrandr --output HDMI-1-0 --auto --output eDP1 --auto --left-of HDMI-1-0
+xrandr --output HDMI-1-0 --auto --output eDP-1 --auto --left-of HDMI-1-0
 xmodmap ~/.Xmodmap
 
 setxkbmap gb
