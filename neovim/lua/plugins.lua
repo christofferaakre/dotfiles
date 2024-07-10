d380f8f86e0518930df0055325a5cf79724f4ea5
caa <caakre@vizrt.com>
2024-07-10 11:00:43 +0200

    updates

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -4,17 +4,17 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'gruvbox-community/gruvbox'
     use { 'numToStr/Comment.nvim' }
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-    use 'nvim-treesitter/nvim-treesitter-context'
+    -- use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    -- use 'nvim-treesitter/nvim-treesitter-context'
     use 'unblevable/quick-scope'
     use 'nvim-lua/plenary.nvim'
     use 'ThePrimeagen/harpoon'
     use { 'nvim-telescope/telescope-fzf-native.nvim', run =
     'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
     use 'nvim-telescope/telescope.nvim'
-    use { "akinsho/toggleterm.nvim", tag = '*', config = function() require("toggleterm").setup() end }
-    --use 'nvim-tree/nvim-tree.lua'
-    --use 'nvim-tree/nvim-web-devicons'
+    -- use { "akinsho/toggleterm.nvim", tag = '*', config = function() require("toggleterm").setup() end }
+    use 'nvim-tree/nvim-tree.lua'
+    use 'nvim-tree/nvim-web-devicons'
     --use 'mbbill/undotree'
     use 'tpope/vim-fugitive'
     use {
@@ -42,6 +42,7 @@ return require('packer').startup(function(use)
     use { 'folke/neoconf.nvim' }
 
     use 'simrat39/rust-tools.nvim'
+    use { "nvim-neotest/nvim-nio" }
     use 'mfussenegger/nvim-dap'
     use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
     use 'theHamsta/nvim-dap-virtual-text'
--- neovim/lua/treesitter.lua
+++ neovim/lua/treesitter.lua
@@ -2,7 +2,7 @@ vim.filetype.add({extension = {wgsl = "wgsl"}})
 
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "cpp", "rust", "python", "javascript", "typescript", "tsx"},
+  ensure_installed = { "c", "cpp", "rust", "python", "javascript", "typescript", "tsx", "lua"},
 
   highlight = {
     enable = true,
