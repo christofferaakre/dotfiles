6fa00f87f640dfed5c6fb0a04f8ab128c0d9ef56
caa <caakre@vizrt.com>
2024-07-10 10:48:32 +0200

    disable tmux

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,6 @@
 if status is-interactive
 starship init fish | source
 set fish_greeting
-
-if status is-interactive
-and not set -q TMUX
-    exec tmux
 end
 
-end
-
-thefuck --alias | source
+#thefuck --alias | source
